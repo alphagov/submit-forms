9a3ce2bb952479f3d645f6e34aeba7e04c9edfb1
Paul Downey <psd@whatfettle.com>
2017-11-20 16:29:28 +0000

    Form sections admin

--- forms/admin.py
+++ forms/admin.py
@@ -1,26 +1,36 @@
 from django.contrib import admin
-from .models import Organisation, Phase, DataType, InputType, Validation, FieldType, Question, Section, Form
+from .models import Organisation, Phase, DataType, InputType, Validation, Field, Question, Section, Form, FormSection
 
 
 admin.site.register(Organisation)
 admin.site.register(Validation)
-admin.site.register(FieldType)
+admin.site.register(Field)
 
 admin.site.register(Question)
 admin.site.register(Section)
-admin.site.register(Form)
+
+
+class FormSectionInline(admin.TabularInline):
+    model = FormSection
+    extra = 1
+
+
+@admin.register(Form)
+class FormAdmin(admin.ModelAdmin):
+    list_display = ['form', 'phase', 'reference', 'name', 'description']
+    inlines = (FormSectionInline,)
 
 
 @admin.register(InputType)
 class InputTypeAdmin(admin.ModelAdmin):
-        list_display = ['inputtype']
+    list_display = ['inputtype']
 
 
 @admin.register(DataType)
 class DataTypeAdmin(admin.ModelAdmin):
-        list_display = ('datatype', 'description')
+    list_display = ('datatype', 'description')
 
 
 @admin.register(Phase)
 class PhaseAdmin(admin.ModelAdmin):
-        list_display = ['phase']
+    list_display = ['phase']
