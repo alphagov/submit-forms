91160fe0832e3792f3331e5f8d78e1b3f5617382
Paul Downey <psd@whatfettle.com>
2017-11-27 13:59:29 +0000

    Minimal form view/preview

--- app/urls.py
+++ app/urls.py
@@ -2,6 +2,7 @@ from django.conf import settings
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.http import HttpResponse
+
 import forms.views
 
 from httpproxy.views import HttpProxy
@@ -12,4 +13,9 @@ urlpatterns = [
 
     url(r'^$', forms.views.home),
     url(r'^admin/', include(admin.site.urls)),
+
+    url(r'^forms/$', forms.views.forms, name='forms'),
+
+    url(r'^form/(?P<key>[\d]{1,16})/$', forms.views.form, name='form'),
+    url(r'^form/(?P<key>[\d]{1,16})/preview/(?P<runner>[\w]{1,256})$', forms.views.preview, name='preview'),
 ]
--- forms/views.py
+++ forms/views.py
@@ -1,5 +1,47 @@
 from django.shortcuts import render
+from django.contrib import admin
+from django.db.models import Count
+from django.http import Http404
+
+from .models import Organisation, Form
+
+admin.autodiscover()
+
+# TBD: plugin architecture / separate runner application ..
+runners = {
+    'elements': {
+        'name': 'GOV.UK form elements',
+        'url': 'http://govuk-elements.herokuapp.com/form-elements/',
+        'media': 'a single web page',
+    }
+}
 
 
 def home(request):
     return render(request, 'home.html')
+
+
+def forms(request):
+    forms = Form.objects \
+        .annotate(n_sections=Count('sections', distinct=True)) \
+        .annotate(n_questions=Count('sections__questions', distinct=True))
+    return render(request, 'forms.html', {'forms': forms})
+
+
+def form(request, key=None, template='form.html'):
+    form = Form.objects.get(form=key)
+    organisations = Organisation.objects.filter(
+        organisation__in=form.organisations.all())
+
+    return render(request, template, {
+        'form': form,
+        'organisations': organisations,
+        'runners': runners,
+    })
+
+
+def preview(request, key=None, runner=None):
+    if runner not in runners:
+        raise Http404("Not found")
+    template = 'runner/%s/preview.html' % (runner)
+    return form(request, key, template)
